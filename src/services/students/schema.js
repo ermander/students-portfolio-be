eecaea0b067d6326236cd87e497b2f5f7af5df02
Emanuele Bertuol <emanuelebertuol@gmail.com>
2020-07-30 10:41:45 +0200

    .

--- src/services/students/schema.js
+++ src/services/students/schema.js
@@ -38,7 +38,7 @@ const studentSchema = new Schema(
           },
        image: {
          type: String,
-         required: true
+         required: false
        },
        projects: [
          {
